4ae852ac868f918d97ca19c5c0f854ef108f3779
anant0059 <2019uce0059@gmail.com>
2023-02-05 14:01:40 +0530

    sign-up page

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public")); //for using all the static files present in the public folder.
+
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
-    console.log("Server is live and running");
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.email;
+
+    console.log(firstName, lastName, email);
+    
 });
 
 app.listen(3000, function(){
